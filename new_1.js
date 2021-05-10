4b2f0f7a9d9050c861325f072f739be38c044b67
Jaewon <jungjaedev@gmail.com>
2021-05-10 20:40:38 +0900

    learn_JS: function

--- new_1.js
+++ new_1.js
@@ -1,1 +1,17 @@
-console.log("dms873");
+// 첫번재종이컵을 뒤집는다
+// 있으면 먹는다
+// 없으면 다음 종이컵을 뒤집는다
+// 종이컵 수만큼 반복 
+
+function multiplyBy2(input) {
+  input = input * 2;
+  return input;
+}
+
+function divideBy2(input) {
+  input = input / 2;
+  return input;
+}
+
+console.log(multiplyBy2(6));
+console.log(divideBy2(6));
