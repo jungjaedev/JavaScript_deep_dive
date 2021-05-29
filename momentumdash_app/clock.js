0a14a0a05ebd33265fa10102b4581e9cda839146
Jaewon <jungjaedev@gmail.com>
2021-05-29 21:42:44 +0900

    Save name in localstorage

---
+++ momentumdash_app/clock.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const $clock = document.querySelector('.clock');
+const $clock__title = $clock.querySelector('.clock__title');
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  $clock__title.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
