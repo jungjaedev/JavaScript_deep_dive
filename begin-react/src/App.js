86b3327aebdee85248bef1fee289e1998ab177ed
Jaewon <jungjaedev@gmail.com>
2021-06-06 21:44:46 +0900

    Learn React from velopert's documnet

---
+++ begin-react/src/App.js
@@ -0,0 +1,14 @@
+import Hello from './Hello';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
