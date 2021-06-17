c5fcc239bf4d911e7e7a8415a0dcd4702154bdfd
Jaewon <jungjaedev@gmail.com>
2021-06-17 19:49:45 +0900

    stack and queue

--- practice_js/main.js
+++ practice_js/main.js
@@ -57,3 +57,85 @@ function findMatryoshka(matryoshka, size) {
   }
   return false;
 }
+
+function browserStack(actions, start) {
+  let prev = [];
+  let next = [];
+  let now = start;
+
+  for (let i = 0; i < actions.length; i++){
+    if(actions[i] === -1 && prev.length > 0){
+      let nowpage = prev.pop(); 
+      next.push(now)
+      now = nowpage
+    } else if(actions[i] === 1 && next.length > 0) {
+      let nowpage2 = next.pop();
+      prev.push(now);
+      now = nowpage2
+    } else {              
+      prev.push(now); [ba]d[c]
+      now = actions[i];
+      next = [];
+    }
+  }
+  return [prev, now, next]
+}
+
+function improveBook(books, speeds) {
+  let days = [];
+  let count = 1;
+  let answer = [];
+  // 100될때까지. 날짜 구하는 반복문
+  for (let i = 0; i < books.length; i++){
+    while(books[i] + speeds[i] * count < 100) {
+      count++;
+    }
+    days.push(count);
+    count = 1
+  } // [ ]
+
+  while (days.length > 0) {
+    f_index = days.findIndex(i => i > days[0])
+    if(f_index === -1) {
+      answer.push(days.length);
+      days.splice(0, days.length);
+    } else {
+      answer.push(f_index);
+      days.splice(0, f_index);
+    }
+  }
+  return answer;
+}
+
+function queuePrinter(bufferSize, capacities, documents) {
+  let work = [];
+  let sec = 0;
+
+  for (let i=0; i < bufferSize; i++){
+    work.push(0);
+  }
+  let sum = 0;
+
+  while(documents.length > 0){
+    work.pop();
+    sum = 0;
+    for(let j=0; j < work.length; j++){
+      sum += work[j];
+    }
+    
+    let temp = documents.shift();
+    if(temp + sum <= capacities){
+      work.unshift(temp);
+    }else{
+      work.unshift(0);
+      documents.unshift(temp)
+    }
+    sec++; 
+  }
+
+   while(work.length){
+      work.pop();
+      sec++
+    }
+  return sec;
+}
