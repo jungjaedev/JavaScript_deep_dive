7c6e4f44ccb53e79bf60a7e3b0f2a1d8a4a0eb70
Jaewon <jungjaedev@gmail.com>
2021-06-23 20:16:42 +0900

    Learn : Promise.all...

--- practice_js/main.js
+++ practice_js/main.js
@@ -410,3 +410,202 @@ getHen() //
   .then(console.log)
   .catch(console.log);
 
+// !코딩앙마!
+
+const f1 = () => {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          resolve("1번 주문 완료");
+      }, 1000);
+  });
+};
+
+const f2 = (message) => {
+  console.log(message);
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          reject(new Error("err..."));
+      }, 3000);
+  });
+};
+
+const f3 = (message) => {
+  console.log(message);
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          resolve("3번 주문 완료");
+      }, 2000);
+  });
+};
+
+f1()
+  .then((resolve) => f2(resolve))
+  .then((resolve) => f3(resolve))
+  .then((resolve) => console.log(resolve))
+  .catch(console.log);
+
+console.log("시작");
+async function order() {
+  try {
+      const result1 = await f1();
+      const result2 = await f2(result1);
+      const result3 = await f3(result2);
+      console.log(result3);
+  } catch (e) {
+      console.log(e);
+      console.log("종료");
+  }
+}
+
+async function getName() {
+  // return "Mike";
+  // return Promise.resolve('Tom')
+  throw new Error("err...");
+}
+
+getName().catch((err) => {
+  console.log(err);
+});
+
+// await
+function getName(name) {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          resolve(name);
+      }, 1000);
+  });
+}
+
+async function showName() {
+  const result = await getName("Mike");
+  console.log(result);
+}
+
+console.log("시작");
+showName();
+
+
+// Promise
+// !코딩앙마!
+const pr = new Promise((resolve, reject) => {
+  setTimeout(() => {
+      resolve("OK");
+  }, 3000);
+});
+
+pr.then(
+  function (result) {
+      console.log(result + " 가지러 가자.");
+  },
+  function (err) {
+      console.log("다시 주문해주세요..");
+  }
+);
+
+// ex1)
+const pr = new Promise((resolve, reject) => {
+  setTimeout(() => {
+      // resolve("OK");
+      reject(new Error("err..."));
+  }, 1000);
+});
+
+console.log("시작");
+pr.then((result) => {
+  console.log(result);
+})
+  .catch((err) => {
+      console.log(err);
+  })
+  .finally(() => {
+      console.log("끝");
+  });
+
+// ex2) 콜백 지옥
+const f1 = (callback) => {
+  setTimeout(function () {
+      console.log("1번 주문 완료");
+      callback();
+  }, 1000);
+};
+
+const f2 = (callback) => {
+  setTimeout(function () {
+      console.log("2번 주문 완료");
+      callback();
+  }, 3000);
+};
+
+const f3 = (callback) => {
+  setTimeout(function () {
+      console.log("3번 주문 완료");
+      callback();
+  }, 2000);
+};
+
+console.log("시작");
+f1(function () {
+  f2(function () {
+      f3(function () {
+          console.log("끝");
+      });
+  });
+});
+
+// Promise Chaining
+
+const f1 = () => {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          // resolve("1번 주문 완료");
+          reject(new Error("xx"));
+      }, 1000);
+  });
+};
+
+const f2 = (message) => {
+  console.log(message);
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          reject("xxx");
+      }, 3000);
+  });
+};
+
+const f3 = (message) => {
+  console.log(message);
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          resolve("3번 주문 완료");
+      }, 2000);
+  });
+};
+
+// Promise.race
+// 하나만 resolve되면 그 resolve를 리턴한다.
+console.time("x");
+Promise.race([f1(), f2(), f3()]) //
+  .then((resolve) => {
+      console.log(resolve);
+      console.timeEnd("x");
+  });
+
+// Promise.all(3초 소요)
+// 다 보여주거나, 다 안보여줄때 사용한다.
+console.time("x");
+Promise.all([f1(), f2(), f3()]) //
+  .then((resolve) => {
+      console.log(resolve);
+      console.timeEnd("x");
+  });
+
+// 6초 소요
+// console.log("시작");
+// f1()
+//     .then((resolve) => f2(resolve))
+//     .then((resolve) => f3(resolve))
+//     .then((resolve) => console.log(resolve))
+//     .catch(console.log)
+//     .finally(() => {
+//         console.log("끝");
+//     });
