c8260214e661f38b3e7618fcd4039bf865ec601f
Jaewon <jungjaedev@gmail.com>
2021-07-04 18:06:09 +0900

    start to make online shop

---
+++ shop/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" class="btn btn-secondary">
+        Secondary
+      </button>
+    </div>
+  );
+}
+
+export default App;
