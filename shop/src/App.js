d3c4abb0334072923e5817086b0efcd9b2e640a4
Jaewon <jungjaedev@gmail.com>
2021-07-14 17:59:48 +0900

    React: shop , add contents

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+/* eslint-disable */
+
+import React, { useState } from 'react';
+import { Container, Navbar, Nav, NavDropdown, Form, Button, FormControl, Jumbotron } from 'react-bootstrap';
 import './App.css';
+import Data from './data';
 
 function App() {
+  let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -24,6 +29,36 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      <Jumbotron className="background">
+        <h1>20% Season Off</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+
+      <div className="container">
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i} key={i} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="100%" />
+      <h4>{props.shoes.title}</h4>
+      <p>
+        {props.shoes.content} & {props.shoes.price}
+      </p>
     </div>
   );
 }
