e2616537828b86221c17ee9ebe40b5200207c03a
Jaewon <jungjaedev@gmail.com>
2021-08-02 20:28:29 +0900

    프로그래머스 알고리즘

--- practice_js/main.js
+++ practice_js/main.js
@@ -1061,3 +1061,21 @@ function solution(phone_number) {
   }
   return answer;
 }
+
+// 프로그래머스 알고리즘
+function solution(numbers) {
+  var answer = [];
+  let tmp = [];
+  for (let i = 0; i < numbers.length;i++){
+      for (let j = i+1; j < numbers.length; j++ ){
+          tmp.push(numbers[i]+numbers[j]);
+      }
+  }
+  tmp.sort((a,b) => a-b )
+  for(let i = 0; i < tmp.length;i++){
+      if(tmp[i] !== tmp[i+1]){
+          answer.push(tmp[i])
+      }
+  }
+  return answer;
+}
