7e5363628ae35bfeac23051de63eee69e909b887
Jaewon <jungjaedev@gmail.com>
2021-08-09 16:57:37 +0900

    알고리즘 구현 테스트

--- practice_js/main.js
+++ practice_js/main.js
@@ -1191,3 +1191,45 @@ function movingStuff(stuff, limit) {
   }
   return cnt;
 }
+
+// [구현] 보드 게임
+/**
+ * N * N의 크기를 가진 보드판 위에서 게임을 하려고 합니다. 게임의 룰은 다음과 같습니다.
+
+좌표 왼쪽 상단(0, 0)에 말을 놓는다.
+말은 상, 하, 좌, 우로 이동할 수 있고, 플레이어가 조작할 수 있다.
+조작의 기회는 딱 한 번 주어지며, 최대 N * 2 번까지 이동할 수 있다.
+조작할 때 U, D, L, R은 각각 상, 하, 좌, 우를 의미하며 한 줄에 띄어쓰기 없이 써야 한다.
+예시: UDDLLRRDRR, RRRRR
+한 번 움직일 때마다 한 칸씩 움직이게 되며, 그 칸 안의 요소인 숫자를 획득할 수 있다.
+방문한 곳을 또 방문해도 숫자를 획득할 수 있다.
+보드 밖을 나간 말은 OUT 처리가 된다.
+칸 안의 숫자는 0 또는 1이다.
+단, 좌표 왼쪽 상단(0, 0)은 항상 0이다.
+획득한 숫자를 합산하여 숫자가 제일 큰 사람이 이기게 된다.
+보드판이 담긴 board와 조작하려고 하는 문자열 operation이 주어질 때, 말이 해당 칸을 지나가면서 획득한 숫자의 합을 구하는 함수를 작성하세요.
+ * 
+ * 
+ */
+function boardGame(board, operation) {
+  let moves = {
+    'U' : [-1, 0],
+    'D' : [1, 0],
+    'L' : [0, -1],
+    'R' : [0, 1],
+  }
+  let X = 0;
+  let Y = 0;
+  // [Y, X]으로 시작
+  // 반복문 (operation.length 만큼)
+  let [moveY, moveX] = [0, 0];
+  let answer = 0;
+  for(let i = 0; i < operation.length; i++) {
+    [moveY, moveX] = moves[operation[i]];
+    Y += moveY;
+    X += moveX;
+    if( Y < 0 || X < 0 || Y > board.length || X > board.length) return 'OUT'
+    answer += board[Y][X];
+  }
+  return answer;
+};
