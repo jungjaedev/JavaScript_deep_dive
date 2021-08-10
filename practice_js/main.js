b86a1e88d853b363949cc46200bfc5f5c4aab56b
Jaewon <jungjaedev@gmail.com>
2021-08-10 17:00:41 +0900

    알고리즘 피보나치

--- practice_js/main.js
+++ practice_js/main.js
@@ -1233,3 +1233,14 @@ function boardGame(board, operation) {
   }
   return answer;
 };
+
+// fibonacci
+let fibonacci = function (n) {
+  const memo = [0, 1];
+  const aux = (n) => {
+    if (memo[n] !== undefined) return memo[n];
+    memo[n] = aux(n - 1) + aux(n - 2);
+    return memo[n];
+  };
+  return aux(n);
+};
