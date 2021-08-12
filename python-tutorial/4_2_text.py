ab888d3c719204cab82a1f56de93d010ced1ebbb
Jaewon <jungjaedev@gmail.com>
2021-08-12 15:11:30 +0900

    python-basic

---
+++ python-tutorial/4_2_text.py
@@ -0,0 +1,12 @@
+print('Hello world')
+print("Hello world")
+print('''
+Hello
+world
+''')
+
+print("'1'+'1'", '1'+'1')
+print('Hello world' * 10)
+print(len(('Hello world' * 10)))
+print("'Hello world'.replace('world','universe')",
+      'Hello world'.replace('world', 'universe'))
