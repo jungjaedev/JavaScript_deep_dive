a80730700715c06ea6dd4b79f5b19cca37b9458f
Jaewon <jungjaedev@gmail.com>
2021-08-13 22:49:42 +0900

    코딩테스트 연습

--- practice_js/main.js
+++ practice_js/main.js
@@ -1192,6 +1192,21 @@ function movingStuff(stuff, limit) {
   return cnt;
 }
 
+const createMatrix = (village) => {
+  const matrix = [];
+  village.forEach((line) => {
+    const row = [];
+    for (let i = 0; i < line.length; i++) row.push(line[i]);
+    matrix.push(row);
+  });
+  return matrix;
+};
+
+const gossipProtocol2 = function (village, num) {
+  // TODO: 여기에 코드를 작성합니다.
+};
+
+
 // [구현] 보드 게임
 /**
  * N * N의 크기를 가진 보드판 위에서 게임을 하려고 합니다. 게임의 룰은 다음과 같습니다.
