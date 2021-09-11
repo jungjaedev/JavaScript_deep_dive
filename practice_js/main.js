ecee9528c00f61b42f0c50fe9ac2d1384d743f14
Jaewon <jungjaedev@gmail.com>
2021-09-11 19:31:24 +0900

    mergeSort 알고리즘

--- practice_js/main.js
+++ practice_js/main.js
@@ -226,8 +226,7 @@
 
 let encoder = new TextEncoder(); // 기본 인코딩은 'utf-8'
 encoder
-  .encode('코')
-  (
+  .encode('코')(
     // Uint8Array(3) [236, 189, 148]
 
     236
@@ -241,3 +240,34 @@ encoder
     148
   )
   .toString(2); // "10010100"
+
+// merge sort
+const merge = function (left, right) {
+  let merged = [];
+  let leftIdx = 0,
+    rightIdx = 0;
+  const size = left.length + right.length;
+
+  for (let i = 0; i < size; i++) {
+    if (leftIdx >= left.length) {
+      merged.push(right[rightIdx]);
+      rightIdx++;
+    } else if (rightIdx >= right.length || left[leftIdx] <= right[rightIdx]) {
+      merged.push(left[leftIdx]);
+      leftIdx++;
+    } else {
+      merged.push(right[rightIdx]);
+      rightIdx++;
+    }
+  }
+  return merged;
+};
+
+const mergeSort = function (arr) {
+  if (arr.length < 2) return arr;
+  const middle = parseInt(arr.length / 2);
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
+  const merged = merge(left, right);
+  return merged;
+};
