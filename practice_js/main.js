a9bc173b0a6c162b4dabcfb9b92e9cc64d823e2c
Jaewon <jungjaedev@gmail.com>
2021-09-14 20:51:00 +0900

    insertion Sort

--- practice_js/main.js
+++ practice_js/main.js
@@ -271,3 +271,24 @@ const mergeSort = function (arr) {
   const merged = merge(left, right);
   return merged;
 };
+
+// 삽입정렬
+const insertionSort = function (arr, transform = item => item) {
+  let sorted = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    if (transform(arr[i]) >= transform(sorted[i - 1])) {
+      sorted.push(arr[i]);
+    } else {
+      for (let j = 0; j < i; j++) {
+        if (transform(arr[i]) <= transform(sorted[j])) {
+          const left = sorted.slice(0, j);
+          const right = sorted.slice(j);
+          sorted = left.concat(arr[i], right);
+          break;
+        }
+      }
+    }
+  }
+
+  return sorted;
+};
