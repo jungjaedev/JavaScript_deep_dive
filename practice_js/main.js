5294d890d9cf67cb3e30df7c8a0bb2279189ddde
Jaewon <jungjaedev@gmail.com>
2021-09-24 20:03:51 +0900

    이진탐색

--- practice_js/main.js
+++ practice_js/main.js
@@ -345,3 +345,15 @@ let fibonacci = function (n) {
   };
   return aux(n);
 };
+
+---
+const binarySearch = function (arr, target) {
+  let left = 0;
+  let right = arr.length-1
+  while (left <= right) {
+    let middle = parseInt((right + left) / 2);
+    if(arr[middle] === target) return middle;
+    target < arr[middle] ? right = middle - 1 : left = middle + 1;
+  }
+  return -1
+};
