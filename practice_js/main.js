9317430d803b2461bc343dbebdb57f8a80610ee9
Jaewon <jungjaedev@gmail.com>
2021-10-02 19:41:57 +0900

    rotatedArraySearch 알고리즘 연습

--- practice_js/main.js
+++ practice_js/main.js
@@ -518,3 +518,35 @@ const inequalityNumber = function (signs) {
   const max = aux(-1, signs, [], digits.reverse(), Array(10).fill(false));
   return max - min;
 };
+
+//rotatedArraySearch
+const rotatedArraySearch = function (rotated, target) {
+  let left = 0,
+    right = rotated.length - 1;
+
+  while (left <= right) {
+    let middle = parseInt((right + left) / 2);
+
+    if (rotated[middle] === target) {
+      return middle;
+    }
+
+    if (rotated[left] < rotated[middle]) {
+      // 왼쪽 절반이 정렬되어 있는 상태
+      if (target < rotated[middle] && rotated[left] <= target) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    } else {
+      // 오른쪽 절반이 정렬되어 있는 상태
+      if (target <= rotated[right] && rotated[middle] < target) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
+    }
+  }
+
+  return -1;
+};
