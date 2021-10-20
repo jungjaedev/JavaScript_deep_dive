9dcd171d3735e34a17350103db695341d455be19
Jaewon <jungjaedev@gmail.com>
2021-10-20 22:24:22 +0900

    auth practice to learn

---
+++ API_Authentication/Phase1/controllers/users.js
@@ -0,0 +1,13 @@
+module.exports = {
+  signUp: async (req, res, next) => {
+    console.log('UsersController.signup() called!');
+  },
+
+  signIn: async (req, res, next) => {
+    console.log('UsersController.signIn() called!');
+  },
+
+  secret: async (req, res, next) => {
+    console.log('UsersController.secret!');
+  },
+};
