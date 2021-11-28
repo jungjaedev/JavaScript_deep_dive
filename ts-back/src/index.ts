60e74ce6168052a60a401ffb08538db5d7e28942
Jaewon <jungjaedev@gmail.com>
2021-11-29 01:20:17 +0900

    react-native

---
+++ ts-back/src/index.ts
@@ -0,0 +1,23 @@
+import { VerifyDomainIdentityResponse } from 'aws-sdk/clients/ses';
+import express from 'express';
+
+const app = express();
+
+interface Params {
+  a: number
+  b: number
+}
+
+type Add = (x: Params) => number;
+
+const add:Add = (x) => {
+    return x.a + x.b;
+}
+
+app.get('/', (req) => {
+  add({a: 1, b: 2});
+})
+
+app.listen(3001, () => {
+  console.log('started')
+})
---
+++ ts-front/app.js
@@ -0,0 +1,4 @@
+let ajax = new XMLHttpRequest();
+
+ajax.open('GET', 'https://api.hnpwa.com/v8/news/1.json', false);
+ajax.send();
