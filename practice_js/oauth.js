580279ddb459116a3289b2cdead9141feef089c3
Jaewon <jungjaedev@gmail.com>
2021-12-04 22:23:49 +0900

    google oauth client,server both updated

--- practice_js/oauth.js
+++ practice_js/oauth.js
@@ -1,26 +1,58 @@
-const { OAuth2Client } = require('google-auth-library');
-const CLIENT_ID = '';
-const client = new OAuth2Client(CLIENT_ID);
-// 구글 로그인
+clientside;
+const google = async () => {
+  const { type, accessToken, user } = await Google.logInAsync({
+    androidClientId: `~~
+    `,
+  });
+  if (type === 'success') {
+    console.log(111111111111111);
+    axios
+      .post(
+        'http://10.0.2.2:80/oauth/signin/',
+        { accessToken, user },
+        {
+          withCredentials: true,
+        }
+      )
+      .then(() => {
+        console.log(2222222222222);
+      });
+  }
+};
+
+// serverside
+const { Users } = require('../../models');
+const { generateAccessToken, sendAccessToken } = require('../tokenFunctions');
+
 module.exports = {
   post: async (req, res) => {
-    let token = req.body.token;
-    // console.log(token);
-    async function verify() {
-      const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: CLIENT_ID,
+    // console.log(req.body);
+    const email = req.body.user.email;
+    const username = req.body.user.givenName;
+
+    const userInfo = await Users.findOne({
+      where: { email },
+    });
+
+    const accessToken = generateAccessToken(req.body.user);
+    sendAccessToken(res, accessToken);
+    // 유저 검색안되면 신규 유저 -> db에 등록
+    if (!userInfo) {
+      await Users.create({
+        email,
+        password: null,
+        username,
       });
-      const payload = ticket.getPayload();
-      const userid = payload['sub'];
-      // console.log(payload)
     }
-    verify()
-      .then(() => {
-        res.cookie('token', token);
-        res.send('success');
-      })
-      .catch(console.error);
+    // console.log(`111`, username, 222, email);
+    // console.log(user);
+    return res.status(201).json({
+      message: 'Google Login Completed',
+      userinfo: {
+        accessToken,
+        username,
+        email,
+      },
+    });
   },
 };
-// npm install google-auth-library --save
