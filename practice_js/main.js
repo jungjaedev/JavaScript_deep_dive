266a22d13a43cb34e57bbfbbedc1ddb6d49a37b1
Jaewon <jungjaedev@gmail.com>
2021-12-12 23:14:24 +0900

    Content page func feature

--- practice_js/main.js
+++ practice_js/main.js
@@ -956,3 +956,121 @@ function ListOfMyPositiveBeans({ navigation, route }) {
 
 																																																																																																																																						export default ListOfMyPositiveBeans;
 
+
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, View, Text, Button, StyleSheet, TextInput, ImageBackground } from 'react-native';
+import styled from 'styled-components/native';
+// import { signOutUser, signinUser, registUser, actionUser } from '../../modules/user';
+// import { useNavigate } from 'react-router';
+// import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Feather } from '@expo/vector-icons';
+
+function BeansContent({ navigation, route }) {
+  // axios.get('http://localhost:80/beans', {
+    //   data: {
+	  //     beans: data.beansInfo,
+	    //   },
+		  //   withCredentials: true,
+		    // });
+			  // console.log(route.params.data);
+
+			    const [datas, setDatas] = useState({});
+
+				  const getData = async () => {
+				      const token = await AsyncStorage.getItem('AccessToken');
+					      const data = await axios.get(`http://ec2-13-209-98-187.ap-northeast-2.compute.amazonaws.com:8080/beans/${route.params.data}`, {
+						        headers: {
+								        ContentType: 'application/json',
+										        authorization: `Bearer ${token}`,
+												      },
+													        withCredentials: true,
+															    });
+																    setDatas(data.data.data);
+																	    // console.log('------data: ', data.data.data);
+																		  };
+
+																		    useEffect(() => {
+																			    getData();
+																				  }, []);
+
+																				    // console.log(datas);
+																					  const goBack = () => {
+																					      // console.log('BACK!!!');
+																						      navigation.goBack();
+																							    };
+
+																								  // Todo : UI + 게시글 수정 기능 구현 + 뒤로가기 버튼
+																								    console.log(datas);
+																									  return (
+																									      <Container>
+																										        <ImageBackgrounds source={require('../../img/background.jpeg')} resizemode="cover">
+																												        <TouchableOpacity
+																														          activeOpacity={0.8}
+																																            style={{ position: 'absolute', marginTop: 30, marginLeft: 10, width: 35, height: 35 }}
+																																			          onPress={goBack}
+																																					          >
+																																							            <Text>
+																																										            <Feather name="arrow-left-circle" size={35} color="black" />
+																																													          </Text>
+																																															          </TouchableOpacity>
+
+																																																	          {datas !== {} ? (
+																																																			            <View>
+																																																						            <View style={{ alignItems: 'center', marginVertical: 40 }}>
+																																																									              {datas.gourdKinds === true ? (
+																																																												                  <Beans source={require('../../img/blueBean.png')} />
+																																																																                ) : (
+																																																																				                <Beans source={require('../../img/redBean.png')} />
+																																																																								              )}
+																																																																											              </View>
+																																																																														              <Header>
+																																																																																	                <Text>{datas.emotions}</Text>
+																																																																																					              <Text>{datas.emotion_level}</Text>
+																																																																																								                {datas.hasOwnProperty('createdAt') ? (
+																																																																																												                <View>
+																																																																																																                  <Text>{datas.createdAt.slice(11, 16)}</Text>
+																																																																																																				                    <Text>{datas.createdAt.slice(0, 10)}</Text>
+																																																																																																									                </View>
+																																																																																																													              ) : null}
+																																																																																																																              </Header>
+																																																																																																																			              <Content>
+																																																																																																																						                <Text>{datas.contents}</Text>
+																																																																																																																										            </Content>
+																																																																																																																													          </View>
+																																																																																																																															          ) : null}
+																																																																																																																																	        </ImageBackgrounds>
+																																																																																																																																			    </Container>
+																																																																																																																																				  );
+																																																																																																																																				  }
+
+																																																																																																																																				  const Container = styled.View`
+																																																																																																																																				    flex: 1;
+																																																																																																																																					  justifycontent: center;
+																																																																																																																																					    alignitems: center;
+																																																																																																																																						`;
+
+																																																																																																																																						const ImageBackgrounds = styled.ImageBackground`
+																																																																																																																																						  height: 100%;
+																																																																																																																																						    width: 100%;
+																																																																																																																																							  opacity: 1;
+																																																																																																																																							  `;
+
+																																																																																																																																							  const Beans = styled.ImageBackground`
+																																																																																																																																							    /* flex: 1; */
+																																																																																																																																								  height: 40px;
+																																																																																																																																								    width: 40px;
+																																																																																																																																									  top: 40px;
+																																																																																																																																									    margin-left: 10px;
+																																																																																																																																										`;
+
+																																																																																																																																										const Header = styled.View`
+																																																																																																																																										  margin-top: 40px;
+																																																																																																																																										    /* align-items: center; */
+																																																																																																																																											`;
+
+																																																																																																																																											const Content = styled.View``;
+
+																																																																																																																																											export default BeansContent;
+
