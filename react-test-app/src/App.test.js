01fa1fd65fc158114e79412910d8218695e32168
Jaewon <jungjaedev@gmail.com>
2022-05-23 23:07:20 +0900

    remove files

--- react-test-app/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('the counter starts at 0', () => {
-  render(<App />);
-  // screen object를 이용해 id 접근
-  const counterElement = screen.getByTestId('counter');
-  expect(counterElement).toHaveTextContent(0);
-});
