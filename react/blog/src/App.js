5d581ff18561522542087072898c311d6c870e3a
Jaewon <jungjaedev@gmail.com>
2022-05-27 23:06:10 +0900

    Add : component

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -35,6 +35,18 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>5월 24일 발행</p>
       </div>
+
+      <Modal></Modal>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
