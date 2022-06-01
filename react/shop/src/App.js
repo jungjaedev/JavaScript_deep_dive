c7f3aa7d34e2aec7ec284316a67eca76eb30e5c8
Jaewon <jungjaedev@gmail.com>
2022-06-02 01:19:18 +0900

    Add : Card component

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -21,23 +21,21 @@ function App() {
       <div className="main-bg" style={{ backgroundImage: 'url(' + bg + ')' }}></div>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img alt="" src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img alt="" src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img alt="" src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].price}</p>
-          </div>
+          {shoes.map((el, idx) => {
+            return <Card shoes={el} i={idx + 1} />;
+          })}
         </div>
       </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img alt="" src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
