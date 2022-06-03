8bfd3165990511d25cfd684f7b9336a6ea9317cd
Jaewon <jungjaedev@gmail.com>
2022-06-03 20:39:46 +0900

    Add : router

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import bg from './img/bg.png';
 import data from './data';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   let [shoes, setShoes] = useState(data);
@@ -15,17 +16,31 @@ function App() {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+
+            <Link to="/">홈</Link>
+            <Link to="/detail">상세페이지</Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg" style={{ backgroundImage: 'url(' + bg + ')' }}></div>
-      <div className="container">
-        <div className="row">
-          {shoes.map((el, idx) => {
-            return <Card shoes={el} i={idx + 1} />;
-          })}
-        </div>
-      </div>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg" style={{ backgroundImage: 'url(' + bg + ')' }}></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((el, idx) => {
+                    return <Card shoes={el} i={idx + 1} />;
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail" element={<div>상세페이지</div>} />
+      </Routes>
     </div>
   );
 }
