7b9dd2f99196031bb0799f47ae67497fdd75bbb4
Jaewon <jungjaedev@gmail.com>
2022-06-07 17:24:53 +0900

    Add : styled components

--- react/shop/src/routes/Detail.js
+++ react/shop/src/routes/Detail.js
@@ -1,13 +1,33 @@
 import { useParams } from 'react-router-dom';
+// import styled from 'styled-components';
+
+// let YellowBtn = styled.button`
+//   background: ${props => props.bg};
+//   color: ${props => (props.bg === 'blue' ? 'white' : 'black')};
+//   padding: 10px;
+// `;
+
+// let NewBtn = styled.button(YellowBtn)`
+//   margin: 20px;
+// `;
+
+// let Box = styled.div`
+//   background: grey;
+//   padding: 20px;
+// `;
 
 function Detail(props) {
   let { id } = useParams();
 
   return (
     <div className="container">
+      {/* <Box>
+        <YellowBtn bg="blue" />
+        <YellowBtn bg="yellow" />
+      </Box> */}
       <div className="row">
         <div className="col-md-6">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          <img alt="" src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
         </div>
         <div className="col-md-6">
           <h4 className="pt-5">{props.shoes[id].title}</h4>
