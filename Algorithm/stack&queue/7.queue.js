1bbb4c7fcc62f9c957982d456efc9bb3f3e55f86
Jaewon <jungjaedev@gmail.com>
2022-07-10 08:34:39 +0900

    Algorithm : queue

---
+++ Algorithm/stack&queue/7.queue.js
@@ -0,0 +1,13 @@
+// 교육과정 설계
+
+function solution(need, plan) {
+  let answer = 'YES';
+  let queue = need.split('');
+  for (let x of plan) {
+    if (queue.includes(x)) {
+      if (x !== queue.shift()) return 'NO';
+    }
+  }
+  if (queue.length > 0) return 'NO';
+  return answer;
+}
