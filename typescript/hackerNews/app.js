19311e11456af843273f128eb44246de645f0a49
jungjaedev <jungjaedev@gmail.com>
2022-11-09 23:56:32 +0900

    Add: template

--- typescript/hackerNews/app.js
+++ typescript/hackerNews/app.js
@@ -17,8 +17,18 @@ function getData(url) {
 function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
-
-  newsList.push("<ul>");
+  let template = `
+    <div>
+      <h1>Hacker News</h1>
+      <ul>
+        {{__news_feed__}}
+      </ul>
+      <div>
+        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+        <a href="#/page/{{__next_page__}}">다음 페이지</a>
+      </div>
+    </div>
+  `;
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
@@ -30,21 +40,19 @@ function newsFeed() {
     `);
   }
 
-  newsList.push("</ul>");
-  newsList.push(`
-    <div>
-      <a href="#/page/${
-        store.currentPage > 1 ? store.currentPage - 1 : 1
-      }">이전 페이지</a>
-      <a href="#/page/${
-        store.currentPage < newsFeed.length / 10
-          ? store.currentPage + 1
-          : newsFeed.length / 10
-      }">다음 페이지</a>
-    </div>
-  `);
+  template = template.replace("{{__news_feed__}}", newsList.join(""));
+  template = template.replace(
+    "{{__prev_page__}}",
+    store.currentPage > 1 ? store.currentPage - 1 : 1
+  );
+  template = template.replace(
+    "{{__next_page__}}",
+    store.currentPage < newsFeed.length / 10
+      ? store.currentPage + 1
+      : newsFeed.length / 10
+  );
 
-  contatiner.innerHTML = newsList.join("");
+  contatiner.innerHTML = template;
 }
 
 function newsDetail() {
--- typescript/hackerNews/dist/app.c328ef1a.js
+++ typescript/hackerNews/dist/app.c328ef1a.js
@@ -134,13 +134,14 @@ function getData(url) {
 function newsFeed() {
   var newsFeed = getData(NEWS_URL);
   var newsList = [];
-  newsList.push("<ul>");
+  var template = "\n    <div>\n      <h1>Hacker News</h1>\n      <ul>\n        {{__news_feed__}}\n      </ul>\n      <div>\n        <a href=\"#/page/{{__prev_page__}}\">\uC774\uC804 \uD398\uC774\uC9C0</a>\n        <a href=\"#/page/{{__next_page__}}\">\uB2E4\uC74C \uD398\uC774\uC9C0</a>\n      </div>\n    </div>\n  ";
   for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push("\n    <li>\n      <a href=\"#/show/".concat(newsFeed[i].id, "\">\n        ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n      </a>\n    </li>\n    "));
   }
-  newsList.push("</ul>");
-  newsList.push("\n    <div>\n      <a href=\"#/page/".concat(store.currentPage > 1 ? store.currentPage - 1 : 1, "\">\uC774\uC804 \uD398\uC774\uC9C0</a>\n      <a href=\"#/page/").concat(store.currentPage < newsFeed.length / 10 ? store.currentPage + 1 : newsFeed.length / 10, "\">\uB2E4\uC74C \uD398\uC774\uC9C0</a>\n    </div>\n  "));
-  contatiner.innerHTML = newsList.join("");
+  template = template.replace("{{__news_feed__}}", newsList.join(""));
+  template = template.replace("{{__prev_page__}}", store.currentPage > 1 ? store.currentPage - 1 : 1);
+  template = template.replace("{{__next_page__}}", store.currentPage < newsFeed.length / 10 ? store.currentPage + 1 : newsFeed.length / 10);
+  contatiner.innerHTML = template;
 }
 function newsDetail() {
   var id = location.hash.substring(7);
@@ -185,7 +186,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49419" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55408" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
