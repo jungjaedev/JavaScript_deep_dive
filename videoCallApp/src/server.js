604b2e287958d887bf1616edcba2634d0833ed81
jungjaedev <jungjaedev@gmail.com>
2022-11-30 21:18:24 +0900

    Add: basic connection browser & server

--- videoCallApp/src/server.js
+++ videoCallApp/src/server.js
@@ -16,4 +16,13 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from the Browser"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("Hello~!");
+});
+
 server.listen(3000, handleListen);
