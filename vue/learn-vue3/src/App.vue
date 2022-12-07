c08497eaaa6e177df702995af33d612bf806b09d
jungjaedev <jungjaedev@gmail.com>
2022-12-08 01:35:49 +0900

    test about style binding

--- vue/learn-vue3/src/App.vue
+++ vue/learn-vue3/src/App.vue
@@ -1,38 +1,9 @@
 <template>
-  <div>
-    <div :style="styleObject">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
-      voluptatum qui quis at, dicta repudiandae vitae suscipit reiciendis ab aut
-      reprehenderit delectus nihil quod iste. Dolorem possimus architecto quo
-      consequuntur!
-    </div>
-    <button @click="fontSize++">+</button>
-    <button @click="fontSize--">-</button>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import { computed, ref } from "vue";
-
-export default {
-  setup() {
-    // const styleObject = reactive({
-    //   color: "red",
-    //   fontSize: "13px",
-    // });
-    const fontSize = ref(13);
-    const styleObject = computed(() => {
-      return {
-        color: "red",
-        fontSize: fontSize.value + "px",
-      };
-    });
-    return {
-      styleObject,
-      fontSize,
-    };
-  },
-};
+export default {};
 </script>
 
 <style lang="scss" scoped></style>
