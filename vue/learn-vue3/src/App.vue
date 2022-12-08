508aa704cf8eadf78ade34a3b851d938d82c70a9
jungjaedev <jungjaedev@gmail.com>
2022-12-08 23:57:47 +0900

    about v-for

--- vue/learn-vue3/src/App.vue
+++ vue/learn-vue3/src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <template v-for="(item, index) in items" :key="index">
+        <li v-if="item.id % 2 === 0">
+          ID: {{ item.id }}인덱스{{ index }}, {{ item.message }}
+        </li>
+      </template>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+import { reactive } from "vue";
+
+export default {
+  setup() {
+    const items = reactive([
+      { id: 1, message: "C++" },
+      { id: 2, message: "HTML" },
+      { id: 3, message: "CSS" },
+      { id: 4, message: "JavaScript" },
+    ]);
+    return { items };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
