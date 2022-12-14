beb18248e2aa6c267d36ed1c1e8ad64abb37a7d6
jungjaedev <jungjaedev@gmail.com>
2022-12-14 22:57:03 +0900

    vue: about v-if and v-for

--- vue/tutorial/basic-problem-4/app.js
+++ vue/tutorial/basic-problem-4/app.js
@@ -2,7 +2,24 @@ const app = Vue.createApp({
   data() {
     return {
       inputClass: "",
+      paragraphIsVisible: true,
+      inputBackground: "",
     };
+  },
+  methods: {
+    toggleParagraphVisibility() {
+      this.paragraphIsVisible = !this.paragraphIsVisible;
+    },
+  },
+  computed: {
+    paraClasses() {
+      return {
+        user1: this.inputClass === "user1",
+        user2: this.inputClass === "user2",
+        visible: this.paragraphIsVisible,
+        hidden: !this.paragraphIsVisible,
+      };
+    },
   },
 });
 
